31237f7c69d428a0ae5e9d182cc63127b6d1bb9a
dhruvv-butterchicken <67152185+dhruvv-butterchicken@users.noreply.github.com>
2021-02-27 03:46:15 -0800

    Add files via upload

---
+++ Linked_List_Python.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 11 13:10:08 2021
+
+@author: dhruv
+"""
+
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+
+class SLinkedList:
+    def __init__(self):
+        self.headval = None
+
+    def listprint(self):
+        printval = self.headval
+        while printval:
+            print (printval.dataval)
+            printval = printval.nextval
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+# Link first Node to second node
+list.headval.nextval = e2
+
+# Link second Node to third node
+e2.nextval = e3
+
+list.listprint()
